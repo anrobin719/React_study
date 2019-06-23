f75cc17ce1c74618bf097424ae58d2ec9405c1d7
안로빈 <anrobin719@gmail.com>
2019-06-24 08:47:53 +0900

    splitting into components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -12,7 +13,7 @@ class App extends Component {
     otherState: "some other value",
     showPersons: false
   };
-
+  
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
@@ -45,45 +46,23 @@ class App extends Component {
 
   render() {
     let persons = null;
-    let btnClass = '';
 
     if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person,index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              value={person.name}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
-          })}
-        </div>
-      );
-      btnClass = classes.Red;
+      persons = <Persons 
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler} />;
     }
-
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.yellow);
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold);
-    }
-
 
     return (
-        <div className={classes.App}>
-          <h1>Hi, I'm React App!</h1>
-          <p className={assignedClasses.join(' ')}>this is really working!</p>
-          
-          <button
-          className={btnClass}
-          onClick={this.togglePersonsHanlder}>Switch Name</button>
+      <div className={classes.App}>
+        <Cockpit
+        showPersons={this.state.showPersons}
+        persons={this.state.persons}
+        clicked={this.togglePersonsHanlder} />
 
-          {persons}
-        </div>
+        {persons}
+      </div>
     );
   }
 }
